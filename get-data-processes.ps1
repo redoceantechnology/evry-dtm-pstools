085ce97e7b1ecb08ba1496a350abcd6339b6ecbc
tutnes <tutnes@gmail.com>
2020-01-07 16:26:54 +0100

    Updated with processes

---
+++ get-data-processes.ps1
@@ -0,0 +1,36 @@
+##
+# Tarjei Utnes januar 2020
+##
+
+. .\config.ps1
+
+$hosts = Invoke-RestMethod -uri $hostsuri -Headers $headers
+$processes = Invoke-RestMethod -uri $processessuri -Headers $headers
+$hostlookup=New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+
+
+# To create a lookup for the host ids and hostnames
+$hosts | foreach {
+    $hostlookup.add($_.entityId,$_.displayName)
+}
+
+$output = New-Object System.Collections.Generic.List[System.Object]
+
+for ($i=0; $i -lt $processes.length; $i++)  {
+    
+    #"fromRelationships": {
+    #  "isProcessOf"
+    $hclient = $processes.fromRelationships.isProcessOf[0]
+    $tmp = $processes[$i].PsObject.Copy()
+    $tmp | add-member -notepropertyname "fromRelationships.isProcessOf" -notepropertyvalue $hostlookup[$hclient]
+    $output.add($tmp)
+    
+}
+
+
+$output | format-table
+
+#$processes.length
+
+$output | export-csv test2.csv -NoTypeInformation
+
